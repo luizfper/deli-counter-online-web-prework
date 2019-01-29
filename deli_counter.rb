76aad37761b56233173a2b3720c1816c860c3a6c
Luiz Pereira <luizfper@gmail.com>
2019-01-29 22:14:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,3 +2,8 @@ def line (customers,customer)
   return "The line is currently empty." if customers.size==0
   place = customers.index(customer)+1
 end
+
+def take_a_number(katz_deli,name)
+  katz_deli<<name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
