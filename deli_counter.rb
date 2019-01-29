d7c086711f764807b39dd416e0a9789a101c3187
Luiz Pereira <luizfper@gmail.com>
2019-01-29 22:29:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,5 @@ end
 
 def now_serving (katz_deli)
   return puts "There is nobody waiting to be served!" if katz_deli.length ==0
-  puts "Currently serving #{katz_deli.shift}.""
+  puts "Currently serving #{katz_deli.shift}."
 end
